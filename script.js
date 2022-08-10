20de54cd742a9ddd9b143bcbb5363a502e7ccacf
Glitch (elderly-mini-brow) <none>
2022-08-10 16:47:42 +0000

    ⌨️🦅 Updated with Glitch

---
+++ script.js
@@ -0,0 +1,17 @@
+const sr = ScrollReveal({
+    duration:500,
+    reset:true,
+    scale:0.5,
+    
+},
+);
+
+
+
+sr.reveal('#image1', {
+    origin: 'top',
+    distance:'50px',
+    scale:1,
+    duration: 1400,
+    reset: false, 
+});
